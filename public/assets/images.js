814c3f83a6ada78bd06f39845dbc42dbdcbd9aa9
Mozibix <opuzmoses@gmail.com>
2024-11-14 20:55:20 +0100

    chores: updated setup

---
+++ public/assets/images.js
@@ -0,0 +1,10 @@
+const basePath = "/assets";
+
+export const assets = {
+  /* SVGS */
+  Logo: `${basePath}/logo.svg`,
+  WhoWeAreBg: `${basePath}/who_we_are_bg.svg`,
+
+  /* PNGS */
+  HeroBg: `${basePath}/hero_bg.png`,
+};
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { assets } from "../public/assets/images";
+import Navbar from "./(components)/Navbar";
+
+function App() {
+  return (
+    <>
+      {/* HEADER */}
+      <Navbar />
+
+      {/* FOOTER */}
+      {/* 
+      <h1 className="border_test">Hello world</h1>
+
+      <img src={assets.Logo} alt="Logo" />
+      <img src={assets.HeroBg} alt="Hero bg" /> */}
+    </>
+  );
+}
+
+export default App;
